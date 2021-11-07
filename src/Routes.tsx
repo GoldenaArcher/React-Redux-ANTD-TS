94f977b1b0b245538083eb3c1eb3063370b79774
GoldenaArcher <lu.han9467@gmail.com>
2021-11-07 13:47:18 -0700

    add private route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
+import AdminDashboard from "./components/admin/AdminDashboard";
+import AdminRoute from "./components/admin/AdminRoute";
+import Dashboard from "./components/admin/Dashboard";
+import PrivateRoute from "./components/admin/PrivateRoute";
 import Home from "./components/core/Home";
 import Shop from "./components/core/Shop";
 import Signin from "./components/core/Signin";
@@ -13,6 +17,8 @@ const Routes = () => {
         <Route path="/shop" component={Shop} />
         <Route path="/signin" component={Signin} />
         <Route path="/signup" component={Signup} />
+        <PrivateRoute path="/user/dashboard" component={Dashboard} />
+        <AdminRoute path="/admin/dashboard" component={AdminDashboard} />
       </Switch>
     </HashRouter>
   );
